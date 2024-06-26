3e8cd5e5fbcee6c86ac0abb495cfec7f307f379d
IMRAN <pimrankhan9009le@gmail.com>
2024-06-26 07:58:05 -0400

    Two integers equal or not check this program

---
+++ TWO_INTEGERS_EQUAL.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int num1,num2;
+	printf("enter the num1 one");
+	scanf("%d",&num1);
+	printf("enter second num2 Two ");
+	scanf("%d",&num2);
+	if(num1==num2)
+	{
+		printf("this is equal%d%d",num1,num2);
+	
+	}
+	else
+	{
+		printf("not eqal%d%d",num1,num2);
+	}
+
+
+
+
+}
